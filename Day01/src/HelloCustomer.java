3385308cbd45530e69115b6c4963d6ec9a83b287
Tharushi Maleesha Devindi Punchihewa <punchihewadevindi@gmail.com>
2025-11-23 16:34:11 +0530

    Servlet

---
+++ Day01/src/HelloCustomer.java
@@ -0,0 +1,36 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/customer")
+public class HelloCustomer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("customer");
+        PrintWriter out = resp.getWriter();
+        out.println("customer");
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("save customer");
+        PrintWriter out = resp.getWriter();
+        out.println("save customer");
+    }
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("update customer");
+        PrintWriter out = resp.getWriter();
+        out.println("update customer");
+    }
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("delete customer");
+        PrintWriter out = resp.getWriter();
+        out.println("delete customer");
+    }
+}
